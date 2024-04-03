95a1971ef7f24163b7669032b24c0747a51b14bd
Christian Toro <christian.toro2@icloud.com>
2024-04-03 17:10:14 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Stopwatch from "./components/stopwatch";
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <h1>Stopwatch</h1>
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
